e2b6b723ba0406ffabf3ef3d2ed36de6367f7b89
Patryk Keska <patrykkeska@Patryks-iMac-Pro.local>
2020-01-19 14:27:41 +0100

    done

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const backgroundPhoto = document.querySelector('.photo-background'); 
 
-const grayScaleSlider = document.querySelector('.grayScaleSlider')
-const blurSlider = document.querySelector('.blurSlider')
-const addPhotoInput = document.querySelector('.addPhoto');
-const colorSLider = document.querySelector('.colorSlider')
-
+const allInputs = document.querySelectorAll('input'); 
 const basicUrl = "./background.jpg";
 
 
 
-// const adjustBlur = (e)=> { 
-    
-//     document.documentElement.style.setProperty('--blur', `${e.target.value}px` )
-// }
-
-
-// const adjustGrayScale = (e)=>{
-//     document.documentElement.style.setProperty('--grayscaleValue', `${e.target.value/10}` )
-    
-//  }
 
 
 
@@ -38,9 +23,9 @@ const basicUrl = "./background.jpg";
 
  }
 
-blurSlider.addEventListener('input', handleInput)
-grayScaleSlider.addEventListener('input', handleInput); 
-addPhotoInput.addEventListener('input', handleInput)
-colorSLider.addEventListener('input', handleInput)
+
+allInputs.forEach(input => input.addEventListener('input', handleInput))
+
+
 
 
