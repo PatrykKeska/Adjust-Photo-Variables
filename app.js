186341791b6c48e20db0624d786173b09b478879
Patryk Keska <patrykkeska@Patryks-iMac-Pro.local>
2020-01-19 13:54:25 +0100

    own url and border color picker added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const backgroundPhoto = document.querySelector('.photo-background');
 const grayScaleSlider = document.querySelector('.grayScaleSlider')
 const blurSlider = document.querySelector('.blurSlider')
 const addPhotoInput = document.querySelector('.addPhoto');
+const colorSLider = document.querySelector('.colorSlider')
 
+const basicUrl = "./background.jpg";
 
 
 
@@ -24,13 +26,21 @@ const addPhotoInput = document.querySelector('.addPhoto');
    
     if(e.target.dataset.key === "blur") return document.documentElement.style.setProperty('--blur', `${e.target.value}px` )
 
-    else if (e.target.dataset.key === "grayScale") return document.documentElement.style.setProperty('--grayscaleValue', `${e.target.value/10}` )
+    if (e.target.dataset.key === "grayScale") return document.documentElement.style.setProperty('--grayscaleValue', `${e.target.value/10}` )
 
-    else if (e.target.dataset.key === "addPhoto")document.documentElement.style.setProperty('--photo', `url(${e.target.value})` )
+
+    if (e.target.dataset.key === "color")  document.documentElement.style.setProperty('--borderColor', `${e.target.value}`)
+
+    if (e.target.dataset.key === "addPhoto") {document.documentElement.style.setProperty('--photo', `url(${e.target.value})` ) } 
+    
+    if (e.target.dataset.key === "addPhoto" && e.target.value === "") {document.documentElement.style.setProperty('--photo', `url(${basicUrl})` ) } 
+    
 
  }
 
 blurSlider.addEventListener('input', handleInput)
 grayScaleSlider.addEventListener('input', handleInput); 
 addPhotoInput.addEventListener('input', handleInput)
+colorSLider.addEventListener('input', handleInput)
+
 
